256c177987733636734e14319486641c85b67daa
saiklonee <saiprithvitech@gmail.com>
2026-01-06 23:32:11 +0530

    add clerk backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/user.routes");
+const { requireAuth } = require("@clerk/express");
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello SaveOurSnippet Backend");
+});
+
+app.use("/api/user", requireAuth(), userRoutes);
 
 module.exports = app;
