546b224f1d78b04091a239d9124aecb2ba98836d
saiklonee <saiprithvitech@gmail.com>
2026-01-06 23:29:11 +0530

    add clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import React from "react";
+import SyncUser from "./components/SyncUser";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <SyncUser />
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default App;
