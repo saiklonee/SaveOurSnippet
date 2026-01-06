53fcc93cff1ce223f0628166f3776dd82ee5f5b7
saiklonee <saiprithvitech@gmail.com>
2026-01-06 23:30:04 +0530

    fix user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkUserId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userFullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
