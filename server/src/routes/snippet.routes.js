5ad44a0f4f610d5d9f0228f35dd737a4cbacea50
saiklonee <saiprithvitech@gmail.com>
2026-01-08 22:21:20 +0530

    complete snippet routes

--- server/src/routes/snippet.routes.js
+++ server/src/routes/snippet.routes.js
@@ -1,5 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
+const {
+  createSnippet,
+  updateSnippet,
+  deleteSnippet,
+  getSnippets,
+  getSnippetById,
+  togglePin,
+  getPinnedSnippets,
+  getSnippetsByCollection,
+} = require("../controllers/snippet.controller");
+
+router.get("/", getSnippets);
+router.post("/", createSnippet);
+router.get("/pinned", getPinnedSnippets);
+router.get("/collection/:collectionId", getSnippetsByCollection);
+router.get("/:id", getSnippetById);
+router.put("/:id", updateSnippet);
+router.delete("/:id", deleteSnippet);
+router.patch("/:id/pin", togglePin);
 
 module.exports = router;
