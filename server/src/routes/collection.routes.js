6725e050c7557e3cfe856e0748a4c812560040bb
saiklonee <saiprithvitech@gmail.com>
2026-01-09 23:52:01 +0530

    completed all routes

--- server/src/routes/collection.routes.js
+++ server/src/routes/collection.routes.js
@@ -8,4 +8,10 @@ const {
   deleteCollection,
 } = require("../controllers/collection.controller");
 
+router.post("/", createCollection);
+router.get("/", getCollections);
+router.get("/:id", getCollectionById);
+router.put("/:id", updateCollection);
+router.delete("/:id", deleteCollection);
+
 module.exports = router;
