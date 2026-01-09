62e495984a77d90681c96054f9d4975eb55bb621
saiklonee <saiprithvitech@gmail.com>
2026-01-09 23:50:27 +0530

    impported all collections controllers

--- server/src/routes/collection.routes.js
+++ server/src/routes/collection.routes.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const {
+  createCollection,
+  getCollections,
+  getCollectionById,
+  updateCollection,
+  deleteCollection,
+} = require("../controllers/collection.controller");
 
 module.exports = router;
